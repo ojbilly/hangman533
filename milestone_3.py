1d803c473b1a96abccac5c4868743bb861502738
OJ <opene89@gmail.com>
2024-12-19 19:48:03 +0000

    This is Milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,25 @@
+word = "apple"
+
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    """
+    Continuously prompts the user for a valid guess and checks it.
+    """
+    while True:
+        guess = input("Guess a letter: ")
+
+        if len(guess) == 1 and guess.isalpha():
+            break  
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess)
+
+ask_for_input()
