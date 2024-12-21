bcdedd204b81d8c49421e47147ed5f6f449a4ee7
OJ <opene89@gmail.com>
2024-12-21 11:54:22 +0000

    Updated milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -2,52 +2,53 @@ import random
 
 class Hangman:
 
-    def __init__(self, word, num_lives=5):
-        self.word = word
-        self.num_lives = num_lives
-        self.word_guessed = ['_' for _ in self.word]  
-        self.num_letters = len(set(self.word))  
-        self.list_of_guesses = []  
+    def __init__(self, secret_word, max_lives=5):
+        self.secret_word = secret_word
+        self.remaining_lives = max_lives
+        self.current_guess_state = ['_' for _ in self.secret_word]  
+        self.remaining_unique_letters = len(set(self.secret_word))  
+        self.guessed_letters = []  
 
-    def guess_letter(self, letter):
-        if letter in self.list_of_guesses:
-            print(f"You already guessed '{letter}'. Try a different letter.")
+    def process_guess(self, guessed_letter):
+        if guessed_letter in self.guessed_letters:
+            print(f"You already guessed '{guessed_letter}'. Try a different letter.")
             return
 
-        self.list_of_guesses.append(letter)
+        self.guessed_letters.append(guessed_letter)
 
-        if letter in self.word:
-            print(f"Good guess! '{letter}' is in the word.")
-            for i in range(len(self.word)):
-                if self.word[i] == letter:
-                    self.word_guessed[i] = letter
-            print(f"Current word: {' '.join(self.word_guessed)}")
+        if guessed_letter in self.secret_word:
+            print(f"Good guess! '{guessed_letter}' is in the word.")
+            for index, character in enumerate(self.secret_word):
+                if character == guessed_letter:
+                    self.current_guess_state[index] = guessed_letter
+            print(f"Current word: {' '.join(self.current_guess_state)}")
 
-            self.num_letters -= 1
+            self.remaining_unique_letters -= 1
         else:
-            self.num_lives -= 1
-            print(f"Wrong guess. You have {self.num_lives} lives left.")
-            print(f"Current word: {' '.join(self.word_guessed)}")
+            self.remaining_lives -= 1
+            print(f"Wrong guess. You have {self.remaining_lives} lives left.")
+            print(f"Current word: {' '.join(self.current_guess_state)}")
 
-def play_game(word_list):
-    num_lives = 5
-    game = Hangman("pearl", num_lives)
+def start_game(word_list):
+    max_lives = 5
+    game_instance = Hangman("pearl", max_lives)
 
     while True:
-        if game.num_lives == 0:
-            print("You lost! The word was:", game.word)
+        if game_instance.remaining_lives == 0:
+            print("You lost! The word was:", game_instance.secret_word)
             break
-        elif game.num_letters > 0:
-            guess = input("Guess a letter: ").lower()
-            if len(guess) == 1 and guess.isalpha():
-                game.guess_letter(guess)
+        elif game_instance.remaining_unique_letters > 0:
+            player_guess = input("Guess a letter: ").lower()
+            if len(player_guess) == 1 and player_guess.isalpha():
+                game_instance.process_guess(player_guess)
             else:
                 print("Invalid input. Please enter a single alphabetical character.")
         else:
             print("Congratulations. You won the game!")
-            print("The word was:", game.word)
+            print("The word was:", game_instance.secret_word)
+            print(f"You finished the game with {game_instance.remaining_lives} lives remaining.")
             break
 
 if __name__ == "__main__":
     word_list = ["pearl"]
-    play_game(word_list)
+    start_game(word_list)
